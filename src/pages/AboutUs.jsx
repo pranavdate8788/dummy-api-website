eac8ce2e2807c3677575bb912cd34e2cd404d15e
pranav date <pranavdate908@gmail.com>
2023-02-28 16:29:52 +0530

    new photo addded

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -9,7 +9,7 @@ const AboutUs = () => {
           <div className="text-2xl xs:text-3xl md:text-4xl">
             <div className="text-gray-500">
               Hello,
-              <br /> I am <span className='text-orange-500'>Jay Pokale</span> <br /> AKA <a href='https://dare2solve.vercel.app/' className='text-orange-500'>Dare2Solve</a>
+              <br /> I am <span className='text-orange-500'>Pranav Date</span>  
             </div>
           </div>
         </div>
@@ -18,7 +18,7 @@ const AboutUs = () => {
             className="max-h-[288px] h-[80vw] md:h-72 max-w-[288px] w-[80vw] md:w-72 flex overflow-hidden"
             style={{ borderRadius: "50% 50% 50% 50% / 30% 30% 70% 70% " }}
           >
-            <img src={'jay.webp'} width={288} height={288}></img>
+            <img src={'photo4.jpg'} width={288} height={288}></img>
           </div>
         </div>
       </div>
@@ -33,10 +33,6 @@ const AboutUs = () => {
                 .typeString("Web Developer")
                 .pauseFor(1000)
                 .deleteAll()
-                .pauseFor(100)
-                .typeString("Mathematician")
-                .pauseFor(1000)
-                .deleteAll()
                 .start();
             }}
             options={{
@@ -49,14 +45,10 @@ const AboutUs = () => {
       <div className="flex items-center flex-col-reverse mx-4 mt-48">
         <div className="flex flex-col justify-center p-2 max-w-3xl lg:text-lg">
           <p className="w-full indent-10 text-justify">
-            Myself Jay Pokale, from Government College of Engineering, Yavatmal. Studying in Mechanical Branch and currently in pursuing my final year.
+     Myself Pranav date, from Government College of Engineering, Yavatmal. Studying in Computer Branch and currently pursuing my final year.
           </p>
-          <p className="w-full indent-10 text-justify">
-            Although I am in Mechanical Branch but I study and practice mostly related to computer. Specially Web Development. I created GitHub repo of my projects. <a className='font-semibold text-orange-600 drop-shadow-md' href="https://github.com/JayPokale">Click here</a> to redirct to my GitHub profile.
-          </p>
-          <p className="w-full indent-10 text-justify">
-           I am the creator of Dare2Solve. <a className='font-semibold text-orange-600 drop-shadow-md' href="https://dare2solve.vercel.app">Click here</a> to know more about Dare2Solve. I gathered more than 20,000 followers due to Dare2Solve. It is typically Mathematics based account where I create several Maths questions and upload their solutions on my website.
-          </p>
+           
+           
         </div>
         <div className="w-full xs:w-2/3 md:w-1/2 lg:w-2/5 p-2">
           <img src={'./AboutUs.gif'} />
